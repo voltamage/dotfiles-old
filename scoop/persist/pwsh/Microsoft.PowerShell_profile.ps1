0187b2cf06449940a814d4fbd8d6e852de8a3d07
voltamage <git@mail.voltamage.com>
2022-02-16 16:52:36 -0800

    updated pwsh and added wt config

--- scoop/persist/pwsh/Microsoft.PowerShell_profile.ps1
+++ scoop/persist/pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,8 @@
 Import-Module PSReadLine
+
+$ENV:EDITOR = "C:\Users\volt\scoop\shims\nvim.exe"
+$ENV:_ZO_DATA_DIR = 'C:\Users\volt\.local\share\zoxide'
+
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineOption -HistoryNoDuplicates
@@ -9,9 +13,6 @@ Set-PSReadLineOption -PredictionSource History
 Invoke-Expression (&scoop-search --hook)
 
 Set-Alias cd z -Option AllScope
-
-$ENV:EDITOR = "C:\Users\volt\scoop\shims\nvim.exe"
-$ENV:_ZO_DATA_DIR = 'C:\Users\volt\.local\share\zoxide'
 
 Invoke-Expression (& {
     $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
